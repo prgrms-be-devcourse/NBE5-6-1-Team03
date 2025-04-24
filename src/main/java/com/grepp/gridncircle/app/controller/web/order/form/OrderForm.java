4b4ed6adc0f65804f51d949bd7087f178cea65af
SEONGYEON JEUNG <godricj1@gmail.com>
2025-04-24 17:41:27 +0900

    feat: order-page complete?

--- src/main/java/com/grepp/gridncircle/app/controller/web/order/form/OrderForm.java
+++ src/main/java/com/grepp/gridncircle/app/controller/web/order/form/OrderForm.java
@@ -5,12 +5,14 @@ import com.grepp.gridncircle.app.model.payment.dto.PaymentDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.Data;
 
 @Data
 public class OrderForm {
 
+    @NotNull
     private List<PaymentDto> menuList;
 
     private String id;
